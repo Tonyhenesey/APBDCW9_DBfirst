7c5f8688dfe2f5265dd62855dfdd9918f736dd66
s27928 <s27928@pjwstk.edu.pl>
2024-06-03 17:43:57 +0200

    Initial Commit

---
+++ APBD__Cwiczenia9/APBD__Cwiczenia9/Controllrs/TripsController.cs
@@ -0,0 +1,35 @@
+﻿using APBD__Cwiczenia9.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBD.Controllrs;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdCw08Context _context;
+
+    public TripsController(ApbdCw08Context context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips
+            .Select(e => new
+            {
+                Name = e.Name,
+                Countries = e.IdCountries.Select(c => new
+                {
+                    Name = c.Name
+                })
+            })
+            .ToListAsync();
+
+        return Ok(trips);
+    }
+
+}
--- APBD__Cwiczenia9/APBD__Cwiczenia9/obj/Debug/net8.0/APBD__Cwiczenia9.AssemblyInfo.cs
+++ APBD__Cwiczenia9/APBD__Cwiczenia9/obj/Debug/net8.0/APBD__Cwiczenia9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD__Cwiczenia9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c6423db0b7cfc27bbbe38094801f2b1b42eac0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ceaba0b1a6920007869518478013d02eb84219a")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD__Cwiczenia9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD__Cwiczenia9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
